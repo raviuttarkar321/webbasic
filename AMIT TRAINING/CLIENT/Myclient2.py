ac6814ccb5d1e7665ec1467108f195038ed3ffce
raviuttarkar321 <36786008+raviuttarkar321@users.noreply.github.com>
2021-06-12 11:01:34 +0530

    Add files via upload

--- AMIT TRAINING/CLIENT/Myclient2.py
+++ AMIT TRAINING/CLIENT/Myclient2.py
@@ -1,12 +1,12 @@
 import socket
-print("hi")
+print("client 2")
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 5000        # The port used by the server
-print("hi")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(s.connect((HOST, PORT)))
-    print("Client one connected")
-    s.sendall(b'Client 1')
+    print("connection successful")
+    s.sendall(b'Client 2')
     data = s.recv(1024)
 
 print('Received', repr(data))
--- AMIT TRAINING/SERVER/server.py
+++ AMIT TRAINING/SERVER/server.py
@@ -1,5 +1,5 @@
 import socket
-
+import time
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 5000        # Port to listen on (non-privileged ports are > 1023)
 print("hello")
@@ -18,4 +18,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s=str(data)
             s=s.upper()
             print(s)
-            conn.sendall(b'HELLOWORD')
+            time.sleep(30)
+            
+            conn.sendall(s.encode())
