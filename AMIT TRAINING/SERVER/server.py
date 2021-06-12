d8e083c5cbc72dd024c4aa8303e28acb469eaa10
raviuttarkar321 <36786008+raviuttarkar321@users.noreply.github.com>
2021-06-12 14:27:03 +0530

    Add files via upload

--- AMIT TRAINING/SERVER/server.py
+++ AMIT TRAINING/SERVER/server.py
@@ -1,23 +1,29 @@
 import socket
 import time
+#import thread
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 5000        # Port to listen on (non-privileged ports are > 1023)
 print("hello")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("no data received")
-                break
-            print(type(data),data)
-            s=str(data)
-            s=s.upper()
-            print(s)
-            time.sleep(30)
+    while True:
+        s.listen()
+
+        while(True):
+            conn, addr = s.accept()
             
-            conn.sendall(s.encode())
+            #thread.start_new_thread(on_new_client,(conn,addr))
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        print("no data received")
+                        break
+                    print(type(data),data)
+                    s=str(data)
+                    s=s.upper()
+                    print(s)
+                    #time.sleep(30)
+                    
+                    conn.sendall(s.encode())
